b441905c1cad32875b5390aaad16a35a62303438
Sergio <you@example.com>
2025-05-19 14:04:26 +0200

    actualizacionHome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,23 @@
-import React from 'react';
-import RpsGame from '../components/Game';
+import React from "react";
+import RpsGame from "../components/Game"
 
 function Home() {
   return (
-    <div className="home-wrapper">
-      <div className="fade-in intro-section">
-        <div className="intro-text">
-          <h1 className="typewriter">Hola, soy Sergio</h1>
-          <p className="typewriter2">Bienvenido a mi portfolio personal. Soy desarrollador web fullstack.</p>
-          <a href="/CV Sergio Checa Alvea.pdf" download className="download-btn fade-in-btn">
-            Descargar CV
-          </a>
-        </div>
+    <div className="fade-in">
+      <h2>Hola, soy Sergio</h2>
+      <p className="p-home">Bienvenido a mi portfolio personal. Soy desarrollador web fullstack.</p>
+      <div style={{ textAlign: 'center' }}>
+        <a href="/CV Sergio Checa Alvea.pdf" download className="download-btn fade-in-btn">
+          Descargar CV
+        </a>
       </div>
-
       <RpsGame />
     </div>
-  );
+  )
 }
 
 export default Home;
 
 
-  
 
-
-
-
-  
   
